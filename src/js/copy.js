7f924f6c75b41c1100e3c52954ca83a1fe9d5c14
Shizuka7 <S_Shizuka@outlook.com>
2024-05-01 19:46:59 +0800

    创建文件

---
+++ src/js/copy.js
@@ -0,0 +1,10 @@
+
+function copyToClipboard(text) {
+  var tempInput = document.createElement("input");
+  tempInput.value = text;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+  alert("链接已复制");
+}
